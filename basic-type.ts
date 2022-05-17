3077c8151f03e0981717c6e5c518bc15f3bb4fed
aliyu <1324149139@qq.com>
2022-05-17 16:28:06 +0800

    基本类型

---
+++ basic-type.ts
@@ -0,0 +1,21 @@
+let isDone: boolean = false;
+
+let age: number = 10;
+
+let firstName: string = "aliyu";
+let message: string = `hello,${firstName}`;
+
+//null表示"没有对象"，即该处不应该有值。
+//undefined表示"缺少值"，就是此处应该有一个值，但是还没有定义
+let u: undefined = undefined
+let n: null = null
+
+let num: number = undefined
+
+let  notSure: any = 4
+notSure = 'maybe a string'
+notSure = true
+
+notSure.myName
+notSure.getName()
+
